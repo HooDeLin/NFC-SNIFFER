ef6eebaa7ff3e9d54ecf373a7950c04d101dffec
HooDeLin <hoodelin0619@gmail.com>
2016-04-20 15:55:05 +0800

    Added remote server json methods

--- app/src/main/java/com/cs3235/nfcsniffer/MainScreen.java
+++ app/src/main/java/com/cs3235/nfcsniffer/MainScreen.java
@@ -24,7 +24,21 @@ import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 
 public class MainScreen extends AppCompatActivity {
@@ -37,14 +51,6 @@ public class MainScreen extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
     }
 
@@ -93,6 +99,13 @@ public class MainScreen extends AppCompatActivity {
             String readResultString = decode(byteToHex(readResult));
             Log.d("readResultHex", byteToHex(readResult));
             Log.d("readResult", readResultString);
+            Map<String, String> result = new HashMap<String, String>();
+            result.put("psehex", byteToHex(selectPSEDirectoryResult));
+            result.put("pse", PSEDirectory);
+            result.put("readresulthex", byteToHex(readResult));
+            result.put("readresult", readResultString);
+            String url = "http://cs3235-2.appspot.com/cardinfo";
+            makeRequest(url, result);
 
         } catch(Exception e) {
             Log.d("nfcDebugError", e.getMessage());
@@ -122,6 +135,75 @@ public class MainScreen extends AppCompatActivity {
         }
         return str;
 
+    }
+
+    public static Object makeRequest(String path, Map params) throws Exception
+    {
+        //instantiates httpclient to make request
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+
+        //url with the post data
+        HttpPost httpost = new HttpPost(path);
+
+        //convert parameters into JSON object
+        JSONObject holder = getJsonObjectFromMap(params);
+
+        //passes the results to a string builder/entity
+        StringEntity se = new StringEntity(holder.toString());
+
+        //sets the post request as the resulting string
+        httpost.setEntity(se);
+        //sets a request header so the page receving the request
+        //will know what to do with it
+        httpost.setHeader("Accept", "application/json");
+        httpost.setHeader("Content-type", "application/json");
+
+        //Handles what is returned from the page
+        ResponseHandler responseHandler = new BasicResponseHandler();
+        return httpclient.execute(httpost, responseHandler);
+    }
+
+    private static JSONObject getJsonObjectFromMap(Map params) throws Exception {
+
+        //all the passed parameters from the post request
+        //iterator used to loop through all the parameters
+        //passed in the post request
+        Iterator iter = params.entrySet().iterator();
+
+        //Stores JSON
+        JSONObject holder = new JSONObject();
+
+        //using the earlier example your first entry would get email
+        //and the inner while would get the value which would be 'foo@bar.com'
+        //{ fan: { email : 'foo@bar.com' } }
+
+        //While there is another entry
+        while (iter.hasNext())
+        {
+            //gets an entry in the params
+            Map.Entry pairs = (Map.Entry)iter.next();
+
+            //creates a key for Map
+            String key = (String)pairs.getKey();
+
+            //Create a new map
+            Map m = (Map)pairs.getValue();
+
+            //object for storing Json
+            JSONObject data = new JSONObject();
+
+            //gets the value
+            Iterator iter2 = m.entrySet().iterator();
+            while (iter2.hasNext())
+            {
+                Map.Entry pairs2 = (Map.Entry)iter2.next();
+                data.put((String)pairs2.getKey(), (String)pairs2.getValue());
+            }
+
+            //puts email and 'foo@bar.com'  together in map
+            holder.put(key, data);
+        }
+        return holder;
     }
 
 
